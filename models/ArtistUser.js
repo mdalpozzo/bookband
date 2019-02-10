99dd1207df1e12268125c30d131796efc73513c1
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2019-02-10 11:52:17 -0800

    creates and exports artist and host user models

--- models/ArtistUser.js
+++ models/ArtistUser.js
@@ -14,5 +14,11 @@ const ArtistUserSchema = new Schema({
   password: {
     type: String,
     required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
   }
 })
+
+module.exports = ArtistUser = mongoose.model('artistusers', ArtistUserSchema);
